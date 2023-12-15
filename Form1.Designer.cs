defc996c450b9d601d6d66666d4d3f68faee11fb
devE <emrecuni0067@gmail.com>
2023-12-15 23:22:52 +0300

    serial port

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,43 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.serialPortVolume = new System.IO.Ports.SerialPort(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(13, 42);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 27);
-            this.numericUpDown1.TabIndex = 1;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // serialPortVolume
             // 
             this.serialPortVolume.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortVolume_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 27);
+            this.textBox1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(398, 282);
-            this.Controls.Add(this.numericUpDown1);
+            this.ClientSize = new System.Drawing.Size(225, 271);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
             this.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.IO.Ports.SerialPort serialPortVolume;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,10 +5,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace SystemVolumeEditer
 {
@@ -18,14 +20,15 @@ namespace SystemVolumeEditer
         {
             InitializeComponent();
         }
-        
+
         CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+        string[] ports;
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
             try
             {
-                defaultPlaybackDevice.Volume = int.Parse(numericUpDown1.Value.ToString());
+                //defaultPlaybackDevice.Volume = int.Parse(numericUpDown1.Value.ToString());
             }
             catch (Exception ex)
             {
@@ -37,7 +40,8 @@ namespace SystemVolumeEditer
         {
             try
             {
-
+                //timer1.Enabled = true;
+                //timer1.Start();
             }
             catch (Exception ex)
             {
@@ -49,13 +53,83 @@ namespace SystemVolumeEditer
         {
             try
             {
-
+                if(serialPortVolume.BytesToRead > 0)
+                {
+                    string data = serialPortVolume.ReadLine();
+                    MessageBox.Show(data);
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("ex.message: " + ex.Message + " stacktrace: " + ex.StackTrace, "Data Received Error");
             }
         }
-        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                ports = SerialPort.GetPortNames();
+                foreach (var port in ports)
+                {
+                    if(comboBox1.Items.IndexOf(port) == -1)
+                        comboBox1.Items.Add(port);
+                    //if(comboBox1.Items.IndexOf(port)!= -1)
+                    //    comboBox1.Items.Remove(port);
+                }
+                //foreach (var item in comboBox1.Items)
+                //{
+                //    if (!ports.Contains(item.ToString()))
+                //        comboBox1.Items.Remove(item);
+                //}
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ex.message: " + ex.Message + " stacktrace: " + ex.StackTrace, "Timer Error");
+            }
+        }
+        private SerialPort serialPort;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //serialPortVolume.PortName = comboBox1.SelectedItem.ToString();
+                //serialPortVolume.Open();
+                string portName = "COM4";
+                int baudRate = 9600;
+
+                // Seri port nesnesini oluştur
+                serialPort = new SerialPort(portName, baudRate);
+
+                // Seri port event'lerini tanımla
+                serialPort.DataReceived += SerialPort_DataReceived;
+                try
+                {
+                    // Seri portu aç
+                    serialPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Seri port açılamadı: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ex.message: " + ex.Message + " stacktrace: " + ex.StackTrace, "Port Selection Error");
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Seri porttan gelen veriyi oku ve TextBox'a yaz
+            string data = serialPort.ReadLine();
+
+            // Bu örnekte TextBox kullanılıyor, uygun bir kontrolü kullanabilirsiniz
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new Action(() => textBox1.Text = data));
+            }
+        }
     }
 }
