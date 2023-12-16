17dd5e7fb135d23dd2c5a496e6c5a91bab0156f2
devE <emrecuni0067@gmail.com>
2023-12-16 20:28:03 +0300

    bug fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -134,6 +134,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bağlantı Yok";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
